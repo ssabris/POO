d90f386d0c0ef7713079192bb8d94406851c4cec
Sabrina Bernardi <125902005+ssabris@users.noreply.github.com>
2026-02-20 20:48:58 -0300

    aula 02

---
+++ aula_02/com.fatec/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Paciente p1 = new Paciente();
+        p1.codigo = 4;
+        System.out.println("Hello, World!");
+    }
+}
